0bb851a0b157a4df315d2488b7dc17ce0b4a20c5
Cengiz ATİLLA <39162812+cengizatilla@users.noreply.github.com>
2022-01-22 11:48:19 +0300

    fix Stepper Type

--- stepper_widget/lib/main.dart
+++ stepper_widget/lib/main.dart
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
               });
             }
           },
-          type: StepperType.horizontal,
+          type: StepperType.vertical,
           steps: getSteps(),
         ),
       ),
